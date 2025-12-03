420116ac86ce96070678f2f69f7076c9cced0a1b
M <frank_hhm@163.com>
2025-12-03 08:51:09 +0800

    1

--- server-api/app/common/jobs/CheckMemberSubscribeJob.php
+++ server-api/app/common/jobs/CheckMemberSubscribeJob.php
@@ -47,7 +47,7 @@ class CheckMemberSubscribeJob
             $filter[] = ["source_id","=",$data["source_id"] ?? 0];
 
             $subscribeSelect = $memberSubscribeService->dao->model->with(["member"])->where($filter)->select()->toArray();
-            dump($data,$subscribeSelect);
+            dump($subscribeSelect);
 
             $mailService = app(MailerService::class);
             $tempRes = MailerHelper::getTemplate("subscribe-message",$data['message']);
