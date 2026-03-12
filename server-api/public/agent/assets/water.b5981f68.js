991e9438ef8ae103962f467ae41685f5f4f6cc11
Frank <Frank_hhm@163.com>
2026-03-12 19:05:31 +0800

    1

---
+++ server-api/public/agent/assets/water.b5981f68.js
@@ -0,0 +1,1 @@
+import{b9 as e}from"./index.12942c33.js";const r=t=>e({url:"finance.water/list",method:"GET",params:t}),a=t=>e({url:"finance.water/agentList",method:"GET",params:t});export{a,r as g};
