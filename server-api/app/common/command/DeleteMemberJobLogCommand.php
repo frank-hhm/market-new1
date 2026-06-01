c1aae9ddf1f5c17394190dbe5dfeb86f73a81d19
Frank <Frank_hhm@163.com>
2026-06-02 02:47:14 +0800

    1

--- server-api/app/common/command/DeleteMemberJobLogCommand.php
+++ server-api/app/common/command/DeleteMemberJobLogCommand.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 namespace app\common\command;
 
 use app\api\services\member\MemberService;
+use app\common\constants\CacheKeyConstant;
 use app\common\helper\ArrayHelper;
 use app\common\helper\ProductHelper;
 use app\common\helper\StringHelper;
@@ -21,6 +22,7 @@ use app\common\services\follow\PersonService;
 use app\common\services\order\OrderLogService;
 use app\common\services\order\OrderPingCangLogService;
 use app\common\services\order\OrderService;
+use Psr\SimpleCache\InvalidArgumentException;
 use think\console\Command;
 use think\console\Input;
 use think\console\Output;
@@ -35,19 +37,23 @@ class DeleteMemberJobLogCommand extends Command
 
     protected function execute(Input $input, Output $output)
     {
-        $cacheService = app(CacheService::class);
+        $cacheService = app(CacheService::class)->setRedisName(CacheKeyConstant::PRODUCT_MARKET_REDIS_DRIVER);
 
         $key = "{queues:CheckMemberSubscribeJob}";
 
-        if($cacheService->has($key)){
-            if($cacheService->delete($key)){
-                echo "CheckMemberSubscribeJob记录----删除成功\n";
-            }else{
+        try {
+            if ($cacheService->has($key)) {
+                if ($cacheService->delete($key)) {
+                    echo "CheckMemberSubscribeJob记录----删除成功\n";
+                } else {
 
-                echo "CheckMemberSubscribeJob记录----删除失败\n";
+                    echo "CheckMemberSubscribeJob记录----删除失败\n";
+                }
+            } else {
+                echo "无CheckMemberSubscribeJob记录\n";
             }
-        }else{
-            echo "无CheckMemberSubscribeJob记录\n";
+        } catch (InvalidArgumentException $e) {
+            echo $e->getMessage()."\n";
         }
         return true;
     }
